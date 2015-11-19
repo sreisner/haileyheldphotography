e5764671aa674d507b8a1066cdcc85e7e11718b2
Shawn Reisner <shawn.reisner@gmail.com>
2015-11-19 14:24:14 -0700

    Add index rewrite initial commit with navbar

---
+++ photography/index2.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css"> 
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+
+        </head>
+    <body>
+        <nav class="navbar navbar-default">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <a class="navbar-brand" href="#">Hailey Held Photography</a>
+                </div>
+                <div class="collapse navbar-collapse navbar-right">
+                    <ul class="nav navbar-nav">
+                        <li><a href="#" class="navbar-link">Gallery</a></li>
+                        <li><a href="#" class="navbar-link">Book Me</a></li>
+                        <li><a href="#" class="navbar-link">About</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </body>
+</html>
