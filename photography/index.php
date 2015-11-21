b60c6aaa8ecb3b79c9fbfdceddba88162afc1cf9
Shawn Reisner <shawn.reisner@gmail.com>
2015-11-21 13:52:31 -0700

    Implement navbar collapse

--- photography/index.php
+++ photography/index.php
@@ -16,11 +16,12 @@
     <head>
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <link rel="stylesheet" href="css/index.css">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
         <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Raleway:400,200'>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css"> 
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+
+        <link rel="stylesheet" href="css/index.css">
 
         <script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
@@ -28,12 +29,19 @@
 
     </head>
     <body onload="onload()">
-        <nav class="navbar navbar-inverse navbar-fixed-top">
+        <div class="navbar navbar-inverse navbar-fixed-top">
             <div class="container-fluid">
                 <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+                            data-target="#navbarHeaderCollapse">
+                         <span class="sr-only">Toggle navigation</span>
+                         <span class="icon-bar"></span>
+                         <span class="icon-bar"></span>
+                         <span class="icon-bar"></span>
+                    </button>
                     <a class="navbar-brand" href="#">Hailey Held Photography</a>
                 </div>
-                <div class="collapse navbar-collapse navbar-right">
+                <div id="navbarHeaderCollapse" class="navbar-collapse collapse navbar-right">
                     <ul class="nav navbar-nav">
                         <li class="dropdown">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown"
@@ -49,13 +57,15 @@
                         </li>
                         <li><a href="book.php" class="navbar-link">Book Me</a></li>
                         <li><a href="about.php" class="navbar-link">About</a></li>
-                        <li><a href="https://www.instagram.com/haileyheld/"><i class="fa fa-instagram"></i></a></li>
-                        <li><a href="https://www.facebook.com/haileyheldphotography/"><i class="fa fa-facebook-square"></i></a></li>
-                        <li><a href="https://www.flickr.com/photos/haileyheld"><i class="fa fa-flickr"></i></a></li>
+                        <li class="navbar-social-links"> 
+                            <a href="https://www.instagram.com/haileyheld/"><i class="fa fa-instagram"></i></a>
+                            <a href="https://www.facebook.com/haileyheldphotography/"><i class="fa fa-facebook-square"></i></a>
+                            <a href="https://www.flickr.com/photos/haileyheld"><i class="fa fa-flickr"></i></a>
+                        </li>
                     </ul>
                 </div>
             </div>
-        </nav>
+        </div>
         
         <div class="container-fluid">
             <div id="image-grid" class="row">
