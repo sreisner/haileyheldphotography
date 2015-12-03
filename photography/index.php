256945cc75ec2f8f7f3ae4a0ee282caaf6e583d3
sreisner <shawn.reisner@gmail.com>
2015-12-03 13:05:08 -0600

    Remove photoswipe, disable save image as

--- photography/index.php
+++ photography/index.php
@@ -15,6 +15,7 @@
 <html lang="en">
     <head>
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta http-equiv="imagetoolbar" content="no" />
 
         <!-- Font Awesome -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
@@ -27,12 +28,6 @@
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
         <script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-
-        <!-- PhotoSwipe -->
-        <link rel="stylesheet" href="css/photoswipe/photoswipe.css">
-        <link rel="stylesheet" href="css/photoswipe/default-skin.css">
-        <script src="js/photoswipe/photoswipe.min.js"></script>
-        <script src="js/photoswipe/photoswipe-ui-default.min.js"></script>
         
         <link rel="stylesheet" href="css/style.css">
 
