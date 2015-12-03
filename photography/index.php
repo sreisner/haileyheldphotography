665316bd4402ea7f4e2b8f99bd4a1c68a0db2a56
sreisner <shawn.reisner@gmail.com>
2015-12-03 13:10:41 -0600

    Add bootstrap skeleton

--- photography/index.php
+++ photography/index.php
@@ -133,5 +133,16 @@
                 </div>
             </div>
         </div>
+        <div class="modal fade" tabindex="-1" role="dialog">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                        <h4 class="modal-title"></h4>
+                    </div>
+                    <div class="modal-body"></div>
+                </div>
+            </div>
+        </div>
     </body>
 </html>
