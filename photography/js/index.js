a5b75fd844dbac5c33a767104c12ce09c2e7b188
sreisner <shawn.reisner@gmail.com>
2015-12-03 13:50:08 -0600

    Add image caption to modal dialog

--- photography/js/index.js
+++ photography/js/index.js
@@ -6,6 +6,7 @@ function registerPhotoPreviewClickHandler() {
     $('#gallery').on('click', '.image-preview-container', function() {
         var image = $(this).find('img').clone();
         $('.modal-body').html(image);
+        $('.modal-title').html($(this).attr("data-caption"));
         $('#imageModal').modal();
     });
 }
