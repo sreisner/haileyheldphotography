440ed3e9d275f09c7448be8c7620d7a4cb6ab10e
sreisner <shawn.reisner@gmail.com>
2015-12-03 13:14:01 -0600

    Move jQuery include to its own head section

--- photography/index.php
+++ photography/index.php
@@ -23,10 +23,12 @@
         <!-- Google Fonts -->
         <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Raleway:400,200'>
 
+        <!-- JQuery -->
+        <script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
+        
         <!-- Bootstrap -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css"> 
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
-        <script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
         
         <!-- Custom Styles -->
