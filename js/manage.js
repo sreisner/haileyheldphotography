fa7f3dc014e08c24695db26d9fc02f913f1f0ab7
sreisner <shawn.reisner@gmail.com>
2015-12-12 16:48:21 -0600

    Implement admin page

---
+++ js/manage.js
@@ -0,0 +1,60 @@
+function onload() {
+    $('#manage-form').hide();
+}
+
+function selectImage(imageContainer) {
+    var caption = $(imageContainer).find('.image-preview-overlay-caption').text();
+    var seriesName = $('#preview-series').find('[selected]').text();
+    $('#edit-series').val(seriesName);
+    $('#edit-caption').val(caption);
+    $('#manage-form').show();
+
+    $('.selected').toggleClass('selected');
+    $(imageContainer).toggleClass('selected');
+    $(imageContainer).find('.image-preview-overlay-container').toggleClass('selected');
+}
+
+function getSelectedPhotoId() {
+    return $('.image-preview-container.selected').attr('data-id');
+}
+
+function deleteClick() {
+    var confirm = window.confirm("Are you sure you want to delete this image?");
+    if(confirm) {
+        deletePhoto(getSelectedPhotoId());
+        setTimeout(function() {
+            location.reload()
+        }, 500);
+    }
+}
+
+function deletePhoto(id) {
+    $.ajax({
+        url: '/img?img_id=' + id,
+        async: false,
+        method: 'DELETE'
+    });
+}
+
+function updateClick() {
+    var series_name = $('#edit-series').val();
+    var caption = $('#edit-caption').val();
+    updatePhoto(getSelectedPhotoId(), series_name, caption);
+
+    setTimeout(function() {
+        location.reload()
+    }, 500);
+}
+
+function updatePhoto(id, series_name, caption) {
+    $.ajax({
+        url: '/img?img_id=' + id + '&caption=' + caption + '&series_name=' + series_name,
+        async: false,
+        method: 'PUT'
+    });
+}
+
+function onPreviewSeriesChange() {
+    var series_name = $('#preview-series').val();
+    window.location.assign('/manage?series_name=' + series_name);
+}
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import urllib
+import pdb
 
 from google.appengine.api import images
 from google.appengine.api import users
@@ -13,6 +14,11 @@ JINJA_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
+
+
+def get_all_series():
+    return Series.query().order(Series.order).fetch()
+
 
 class Series(ndb.Model):
     name = ndb.StringProperty()
@@ -30,18 +36,16 @@ class Photo(ndb.Model):
 class MainPage(webapp2.RequestHandler):
     def get(self):
         series_name = self.request.get('series_name')
-        all_series = Series.query().order(Series.order).fetch()
+
         if series_name:
             photos_query = Photo.query(Photo.series.name == series_name).order(-Photo.uploaded)
         else:
-            series_name = None
             photos_query = Photo.query().order(-Photo.uploaded)
-        photos = photos_query.fetch()
 
         template_values = {
             'series_name': series_name,
-            'all_series': all_series,
-            'photos': photos
+            'all_series': get_all_series(),
+            'photos': photos_query.fetch()
         }
 
         template = JINJA_ENVIRONMENT.get_template('galleries.html')
@@ -49,55 +53,124 @@ class MainPage(webapp2.RequestHandler):
 
 
 class Upload(webapp2.RequestHandler):
-  def get(self):
-    template = JINJA_ENVIRONMENT.get_template('upload.html')
-    error_message = self.request.get('error_message')
-    message = self.request.get('message')
-    all_series = Series.query().order(Series.order).fetch()
-    template_values = {
-        'all_series': all_series,
-        'message': message,
-        'error_message': error_message
-    }
-    self.response.write(template.render(template_values))
+    def get(self):
+        message = self.request.get('message')
+        error_message = self.request.get('error_message')
 
-  def post(self):
+        template_values = {
+            'all_series': get_all_series(),
+            'message': message,
+            'error_message': error_message
+        }
+
+        template = JINJA_ENVIRONMENT.get_template('upload.html')
+        self.response.write(template.render(template_values))
+
+
+class Manage(webapp2.RequestHandler):
+    def get(self):
         series_name = self.request.get('series_name')
-        caption = self.request.get('caption')
-        picture = self.request.get('picture')
+        series = get_all_series()
 
-        series = Series.query(Series.name == series_name)[0]
-        picture = self.request.get('picture')
-        preview = images.resize(picture, 32, 32)
+        if not series_name:
+            series_name = series[0].name
+        
+        photos_query = Photo.query(Photo.series.name == series_name).order(-Photo.uploaded)
 
-        photo = Photo()
-        photo.series = series
-        photo.caption = caption
-        photo.picture = picture
-        photo.preview = preview
-        photo.put()
+        template_values = {
+            'series_name': series_name,
+            'all_series': get_all_series(),
+            'photos': photos_query.fetch()
+        }
 
-        self.redirect('/admin')
-
+        template = JINJA_ENVIRONMENT.get_template('manage.html')
+        self.response.write(template.render(template_values))
 
 class Admin(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Admin')
+        template = JINJA_ENVIRONMENT.get_template('admin.html')
+        self.response.write(template.render({}))
 
 
 class Image(webapp2.RequestHandler):
     def get(self):
-        photo_key = ndb.Key(urlsafe=self.request.get('img_id'))
-        photo = photo_key.get()
+        try:
+            photo_key = ndb.Key(urlsafe=self.request.get('img_id'))
+            photo = photo_key.get()
+        except Exception, e:
+            self.redirect('http://dummyimage.com/600x400/000000/ffffff.jpg&text=image+not+found')
+            return
+
+        is_preview = self.request.get('is_preview')
+
+        self.response.headers['Content-Type'] = 'image/jpg'
+        if(is_preview and photo.preview):
+            self.response.out.write(photo.preview)
         if photo.picture:
-            self.response.headers['Content-Type'] = 'image/jpg'
             self.response.out.write(photo.picture)
-        else:
-            self.response.out.write('No image')
+
+    def post(self):
+        try:
+            self.validate_post(self.request)
+
+            series_name = self.request.get('series_name')
+            caption = self.request.get('caption')
+            picture = self.request.get('picture')
+
+            series = Series.query(Series.name == series_name).fetch()[0]
+            preview = images.resize(picture, 400)
+
+            photo = Photo()
+            photo.series = series
+            photo.caption = caption
+            photo.picture = picture
+            photo.preview = preview
+            photo.put()
+
+            query_params = urllib.urlencode({'message': 'Image uploaded successfully.'})
+        except Exception, e:
+            query_params = urllib.urlencode({'error_message': e})
+
+        self.redirect('/upload?%s' % query_params)
+
+    def validate_post(self, request):
+        series_name = self.request.get('series_name')
+        caption = self.request.get('caption')
+        picture = self.request.get('picture')
+
+        if not picture:
+            raise Exception('You must select a photo to upload.')
+
+        if not series_name:
+            raise Exception('Each photo must have a series.')
+
+        if not caption:
+            raise Exception('Each photo must have a caption.')
+
+    def delete(self):
+        img_id = self.request.get('img_id')
+        if img_id:
+            photo_key = ndb.Key(urlsafe=img_id)
+            photo_key.delete()
+
+    def put(self):
+        img_id = self.request.get('img_id')
+        series_name = self.request.get('series_name')
+        caption = self.request.get('caption')
+
+        series = Series.query(Series.name == series_name).fetch()[0]
+
+        photo_key = ndb.Key(urlsafe=img_id)
+        photo = photo_key.get()
+        photo.series = series
+        photo.caption = caption
+        photo.put()
+
 
 app = webapp2.WSGIApplication([
     ('/', MainPage),
     ('/upload', Upload),
+    ('/manage', Manage),
     ('/admin', Admin),
     ('/img', Image),
 ], debug=True)
